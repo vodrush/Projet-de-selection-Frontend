4f762eeb04697e61ccd6cf56c66dd8f66f2ded26
vodrush <raphaelsantiago3883@gmail.com>
2025-09-01 05:30:20 +0200

    v1.2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NOM-DE-VOTRE-REPO-FRONTEND/',
+  base: '/Projet-de-selection-Frontend/',
 })
